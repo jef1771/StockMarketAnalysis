dd582bd3d900082213d3eba6114e5c5fc576d989
jef1771 <james.ferris1996@gmail.com>
2018-11-10 17:55:34 -0500

    paths

--- Stocks_Java/src/MainSequential.java
+++ Stocks_Java/src/MainSequential.java
@@ -4,12 +4,16 @@ import edu.rit.pj2.Task;
 
 public class MainSequential extends Task {
 	
+	public String Li_Tardis = "/home/stu2/s19/zxl3946/Desktop/StockMarketAnalysis/Stocks_Java/stock_database/A.txt";
+	public String James_Tardis = "/home/stu10/s5/jef1771/Courses/CS654/stocks/StockMarketAnalysis/Stocks_Java/stock_database/A.txt";
+	public String James_Personal = "/Users/Yamie/Desktop/CS654/Stock_Analytics/StockMarketAnalysis/Stocks_Java/stock_database/A.txt";
+
 	public void main(String[] arg0) throws Exception 
 	{				
 		String dir = System.getProperty("user.dir");
         System.out.println("current dir = " + dir);
 		System.out.println("Hello World");
-		Stock s = new Stock("Sample", "/home/stu2/s19/zxl3946/Desktop/StockMarketAnalysis/Stocks_Java/stock_database/A.txt");
+		Stock s = new Stock("Sample", James_Tardis);
 	}
 	
 	/**
